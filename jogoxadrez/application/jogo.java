f6e7186f99590a140beae6191b293b439229e982
Bruno Augusto Paris <bruno.pariss@hotmail.comgit config --global user.email bruno.pariss@hotmail.com>
2021-04-08 01:26:38 -0300

    Implement Board and Piece

--- jogoxadrez/application/jogo.java
+++ jogoxadrez/application/jogo.java
@@ -1,10 +1,15 @@
 package application;
 
+import boardgame.Board;
+
 public class jogo {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello Word");
+		Board board = new Board(8, 8);
+		
+		
+		
 	}
 
 }
