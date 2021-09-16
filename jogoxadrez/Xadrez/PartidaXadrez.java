1adee64856a88bcf870455f8f62d8f6efb0d8f47
Bruno Augusto Paris <bruno.pariss@hotmail.com>
2021-09-16 02:10:00 -0300

    Atualizado projeto de xadrez

---
+++ jogoxadrez/Xadrez/PartidaXadrez.java
@@ -0,0 +1,15 @@
+package Xadrez;
+
+import tabuleiro.Tabuleiro;
+
+public class PartidaXadrez {
+	
+	private Tabuleiro tabuleiro;
+	
+	public PartidaXadrez() {
+		tabuleiro = new Tabuleiro(8, 8);
+	}
+	
+	 
+
+}
---
+++ jogoxadrez/Xadrez/PecaXadrez.java
@@ -0,0 +1,5 @@
+package Xadrez;
+
+public class PecaXadrez {
+
+}
